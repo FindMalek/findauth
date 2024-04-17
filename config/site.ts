99f75861a650967e2776a199d21b087d1f0f5448
FindMalek <malekgarahellalbus@gmail.com>
2024-04-17 21:44:54 +0100

    chore(seo): added layout & open-graph data

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "types"
+
+export const siteConfig: SiteConfig = {
+  name: "FindPlate",
+  description:
+    "FindPlate is a food ordering platform that allows you to order food from your favorite restaurants and have it delivered to your doorstep.",
+  url: "https://plate.findmalek.com",
+  ogImage: "https://plate.findmalek.com/og.png",
+  links: {
+    twitter: "https://twitter.com/foundmalek",
+    github: "https://github.com/findmalek/findplate",
+  },
+}
