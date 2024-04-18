e2a7edbe458dd6de4699f6668655e10ac8a176c9
FindMalek <malekgarahellalbus@gmail.com>
2024-04-18 01:59:29 +0100

    feat(i18n): setup internationalization

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 import createNextIntlPlugin from "next-intl/plugin"
 
-const withNextIntl = createNextIntlPlugin("@/lib/i18n.ts")
+const withNextIntl = createNextIntlPlugin("./lib/i18n.ts")
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
